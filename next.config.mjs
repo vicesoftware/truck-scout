cda7754afff683b14d434445f248757a37bc45de
Ryan Vice <ryan@vicesoftware.com>
2024-09-30 09:53:12 -0500

    Attempting to fix DO deployment using approach POC'd with simple nextjs app

--- next.config.mjs
+++ next.config.mjs
@@ -4,25 +4,6 @@ const nextConfig = {
     DATABASE_URL: process.env.DATABASE_URL,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
-  images: {
-    domains: ['monkfish-app-xxdwd.ondigitalocean.app'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-    unoptimized: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  output: 'standalone',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   assetPrefix: '.',
 };
 
