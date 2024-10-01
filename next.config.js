5e0f7ee97f468427f73767e7cf59ae9c1a3f0002
Ryan Vice <ryan@vicesoftware.com>
2024-10-01 13:56:36 -0500

    Ignoring server side modules on the client side

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        dns: false,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
