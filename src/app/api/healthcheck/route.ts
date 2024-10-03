9470192519ec93d3a530df4b9610d5c720e00d67
Ryan Vice <ryan@vicesoftware.com>
2024-10-02 19:40:05 -0500

    Added logging to help debug database cert issue

--- src/app/api/healthcheck/route.ts
+++ src/app/api/healthcheck/route.ts
@@ -7,24 +7,41 @@ async function checkDatabaseConnection() {
     return false;
   }
 
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      ca: process.env.CA_CERT,
-      rejectUnauthorized: true,
-    },
-  });
+  let pool = null;
 
   try {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          ca: process.env.CA_CERT,
+          rejectUnauthorized: true,
+        },
+      });
+    
     const client = await pool.connect();
     await client.query('SELECT 1');
     client.release();
     return true;
   } catch (error) {
-    console.error('Database connection error:', error);
-    return false;
+    const errorMessage = error instanceof Error ? 
+        error.message : 'An unknown error occurred';
+
+    if (error instanceof Error) {
+        const response = { 
+            error: errorMessage,
+            variables: {
+                DATABASE_URL: process.env.DATABASE_URL,
+                CA_CERT: process.env.CA_CERT
+            }
+        };
+        console.error('Database connection error:', response);
+        return response;
+    }
+    return { error: 'An unknown error occurred' };
   } finally {
-    await pool.end();
+    if (pool) {
+      await pool.end();
+    }
   }
 }
 
